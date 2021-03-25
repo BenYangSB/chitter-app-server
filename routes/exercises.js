5569e49a48e4a1e546c52a0ab2af27d6d36ffcab
Kevin Zhang <kevinzhang425@gmail.com>
2021-03-25 00:25:42 -0700

    Added find myRecipes for user

--- routes/exercises.js
+++ routes/exercises.js
@@ -46,6 +46,11 @@ router.route('/feed/:id').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/myRecipes/:id').get((req, res) => {
+  Exercise.find({userKey : req.params.id})
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: '+ err));
+});
 
 router.route('/:id').delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
